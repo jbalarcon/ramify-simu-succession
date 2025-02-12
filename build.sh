8313612e0ea8fcbed834f6e3c93b9759bf571858
jean-baptiste <jean-baptiste@jbalarcon.com>
2025-02-12 18:41:24 +0100

    fix: improve static asset handling for GitHub Pages

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set repository name for local builds
+export GITHUB_ACTIONS=true
+export GITHUB_REPOSITORY="jbalarcon/ramify-simu-succession"
+
 # Temporarily rename ESLint config
 mv .eslintrc.json .eslintrc.json.bak
 
